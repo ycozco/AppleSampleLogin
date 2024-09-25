7361566516d896c6e1b9d9523fac4d93e94f3e7d
yoset <ycozco@unsa.edu>
2024-09-25 08:37:29 -0700

    first upload

---
+++ SampleLogin	/CreateAccountView.swift
@@ -0,0 +1,18 @@
+//
+//  CreateAccountView.swift
+//  SampleLogin
+//
+//  Created by epismac on 25/09/24.
+//
+
+import SwiftUI
+
+struct CreateAccountView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CreateAccountView()
+}
--- SampleLogin	/LoginView .swift
+++ SampleLogin	/LoginView .swift
@@ -8,15 +8,38 @@
 import SwiftUI
 
 struct LoginView: View {
-    @State private var username=""
-    @State private var password=""
-    
+    @Binding var isLoggedin: Bool
+    @State private var username = ""
+    @State private var password = ""
+
     var body: some View {
-        Image(.login)
-        TextField("Username", text: $username).textFieldStyle(.roundedBorder)
-        TextField("Password", text: $password).textFieldStyle(.roundedBorder)
-        Button("Login", action: {})
+        NavigationView {
+            VStack {
+                Image("login")
+                    .resizable()
+                    .frame(maxWidth: 100, maxHeight: 100)
+                    .scaledToFit()
+                
+                TextField("Username", text: $username)
+                    .textFieldStyle(.roundedBorder)
+                
+                TextField("Password", text: $password)
+                    .textFieldStyle(.roundedBorder)
+                    .textContentType(.password) // Optional: to use the password field style
+                
+                Button("Login", action: btnLogin)
+                
+                NavigationLink(destination: CreateAccountView()) {
+                    Text("Create my account")
+                }
+            }
+            .navigationTitle("Login")
+            .navigationBarTitleDisplayMode(.inline)
+            .navigationBarHidden(true)
+        }
+    }
+
+    func btnLogin() {
+        isLoggedin = true
     }
 }
-
-	
